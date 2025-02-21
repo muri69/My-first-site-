8e6b9b28e4ea6382ca13090f2c7427f135cdb7df
master69 <167116703+muri69@users.noreply.github.com>
2025-02-21 14:36:41 +0300

    Create script.js
    Signed-off-by: master69 <167116703+muri69@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Mesaj trimis! Vă vom contacta în curând.");
+        form.reset();
+    });
+});
