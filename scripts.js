4b7224135341c18c3b1179caf1e6982021a3e41c
master69 <167116703+muri69@users.noreply.github.com>
2025-03-01 13:33:50 +0300

    Update scripts.js
    Signed-off-by: master69 <167116703+muri69@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 // scripts.js
 document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('nav ul li a');
+    const hamburger = document.querySelector('.hamburger');
+    const navUl = document.querySelector('nav ul');
 
+    // Funcționalitate pentru meniul hamburger
+    hamburger.addEventListener('click', function () {
+        navUl.classList.toggle('active');
+    });
+
+    // Funcționalitate pentru link-uri cu scroll smooth
+    const navLinks = document.querySelectorAll('nav ul li a');
     navLinks.forEach(link => {
         link.addEventListener('click', function (e) {
             e.preventDefault();
@@ -9,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector(targetId).scrollIntoView({
                 behavior: 'smooth'
             });
+            navUl.classList.remove('active'); // Închide meniul după click
         });
     });
 });
